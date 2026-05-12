fa989d5337bf15da60cbd27e8b731bf2c0a91258
tsquares60653 <thompson.tia@gmail.com>
2026-05-12 15:55:47 -0500

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import VirtualGalleryWalk from './VirtualGalleryWalk';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <VirtualGalleryWalk />
+  </React.StrictMode>
+);
